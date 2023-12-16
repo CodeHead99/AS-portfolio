986bcf443b45f61b10bd48e43f4d4cace8fdbd46
CodeHead99 <akshitasg08@gmail.com>
2023-12-16 16:58:22 +0530

    Home Page complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Head from "next/head";
 import Image from "next/image";
 import profilePic from "../../public/images/profile/developer-pic-1.png";
 import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
+import HireMe from "@/components/HireMe";
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 
 export default function Home() {
   return (
@@ -22,15 +26,41 @@ export default function Home() {
                 text={"Turning Vision Into Reality With Code And Design."}
                 className="!text-6xl !text-left"
               />
-              <p>
+              <p className="my-4 text-base font-medium ">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/dummy.pdf"
+                  target={"_blank"}
+                  download={true}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
+                >
+                  Resume
+                  <LinkArrow className={"w-6 ml-1"} />
+                </Link>
+                <Link
+                  href="mailto:akshitasg08@gmail.com"
+                  target={"_blank"}
+                  className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
+        <HireMe />
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image
+            src={lightBulb}
+            alt="akshita"
+            className="w-full h-auto"
+          ></Image>
+        </div>
       </main>
     </>
   );
